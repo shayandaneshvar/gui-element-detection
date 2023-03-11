d14aec2d23f314b3dcbb8a3ad521cbbc6503efcb
shayandaneshvar <daneshvarshayan@gmail.com>
2023-03-11 02:21:27 -0600

    cleaned and transform data to YOLO5 format

--- src/constants.py
+++ src/constants.py
@@ -10,11 +10,38 @@ VINS_IPHONE = f"{VINS_DATASET_PATH}Iphone"
 VINS_UPLABS = f"{VINS_DATASET_PATH}uplabs"
 VINS_WIREFRAMES = f"{VINS_DATASET_PATH}Wireframes"
 
-## what was done in the yolov5 paper and the paper before that?
+# what was done in the yolov5 paper and the paper before that? all but the wireframes
 
 VINS_MERGED = f"{VINS_DATASET_PATH}Merged"
 VINS_MERGED_IMAGES = f"{VINS_MERGED}/images/"
 VINS_MERGED_ANNOTATIONS = f"{VINS_MERGED}/annotations/"
+VINS_MERGED_YOLO = f"{VINS_MERGED}/yolo5_format/"
+# EditText -> InputField
+# CheckedTextView -> CheckedView
+# Drawer -> Sliding Menu
+# Modal -> Pop-Up window
+# Discarded Rare Items: Spinner, Card, Multi tab, Toolbar, Bottom_Navigation, Remember
+
+# Assholes who came up with VINS, updated the names !!! WTF! Wasted 3 hours finding wtf is going on!
+VINS_CLASSES = {'BackgroundImage': 0, 'CheckedTextView': 1, 'Icon': 2, 'EditText': 3,
+                'Image': 4, 'Text': 5, 'TextButton': 6, 'Drawer': 7, 'PageIndicator': 8,
+                'UpperTaskBar': 9, 'Modal': 10, 'Switch': 11}
+
+VINS_CLASSES_COUNT = {'BackgroundImage': 0, 'CheckedTextView': 0, 'Icon': 0, 'EditText': 0,
+                      'Image': 0, 'Text': 0, 'TextButton': 0, 'Drawer': 0, 'PageIndicator': 0,
+                      'UpperTaskBar': 0, 'Modal': 0, 'Switch': 0}
+
+
+def reset_classes_counts():
+    global VINS_CLASSES_COUNT
+    for key in VINS_CLASSES_COUNT.keys():
+        VINS_CLASSES_COUNT[key] = 0
+
+
+# Final Count:
+
+# YOLOv5 Format per line:
+# (class x_center y_center width height) classes start from 0, the rest are normalized
 
 if __name__ == '__main__':
     images = glob.glob(VINS_MERGED_IMAGES + '*.jpg')
